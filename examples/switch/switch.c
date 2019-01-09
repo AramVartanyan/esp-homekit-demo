f62946baaa1d698262e5012baf532e13c9f935af
AVartanyn <42091996+AramVartanyan@users.noreply.github.com>
2019-01-09 22:10:06 +0200

    Update switch.c
    Sonoff new PCB

--- examples/switch/switch.c
+++ examples/switch/switch.c
@@ -1,13 +1,5 @@
 /*
- * Example of using esp-homekit library to control
- * a switch with chinees mInI D1 USB using HomeKit.
- * The esp-wifi-config library is also used in this
- * example. This means you don't have to specify
- * your network's SSID and password before building.
- *
- * WARNING: Never connect the device to AC while it's
- * connected to the computer port. This may fry them all.
- *
+ * Switch Sonoff new PCB
  */
 
 #include <stdio.h>
@@ -23,113 +15,83 @@
 #include <homekit/characteristics.h>
 #include <wifi_config.h>
 
-#include "button.h"
+#include "adv_button.h"
 
-// The GPIO pin that is connected to the relay on the mInI D1 (D0 pin).
-const int relay_gpio = 16;
-// The GPIO pin that is connected to the Built-in LED on the mInI D1 (D4 pin).
-const int led_gpio = 2;
-// The GPIO pin that is oconnected to an external button (D3 pin).
-const int button_gpio = 0;
+#ifndef BUTTON_GPIO
+#define BUTTON_GPIO 0
+#endif
 
-bool out;
+#define RELAY_GPIO  12
+#define LED_GPIO    13
 
 void switch_on_callback(homekit_characteristic_t *_ch, homekit_value_t on, void *context);
-void button_callback(uint8_t gpio, button_event_t event);
 
-void relay_write(bool on) {
-    gpio_write(relay_gpio, on ? 1 : 0);
+void relay_write(bool on, int gpio) {
+    gpio_write(gpio, on ? 1 : 0);
 }
 
-void led_write(bool on) {
-    gpio_write(led_gpio, on ? 0 : 1);
+void led_write(bool on, int gpio) {
+    gpio_write(gpio, on ? 0 : 1);
 }
 
 void reset_configuration_task() {
-    //Flash the LED first before we start the reset
+    //Flash the LED before the reset
     for (int i=0; i<3; i++) {
-        led_write(true);
+        led_write(true, LED_GPIO);
         vTaskDelay(100 / portTICK_PERIOD_MS);
-        led_write(false);
+        led_write(false, LED_GPIO);
         vTaskDelay(100 / portTICK_PERIOD_MS);
     }
-
     printf("Resetting Wifi Config\n");
-
     wifi_config_reset();
-
     vTaskDelay(1000 / portTICK_PERIOD_MS);
-
     printf("Resetting HomeKit Config\n");
-
     homekit_server_reset();
-
     vTaskDelay(1000 / portTICK_PERIOD_MS);
-
     printf("Restarting\n");
-
     sdk_system_restart();
-
     vTaskDelete(NULL);
 }
 
-void reset_configuration() {
-    printf("Resetting mInI configuration\n");
+void reset_configuration(const uint8_t gpio) {
+    printf("Resetting device configuration\n");
     xTaskCreate(reset_configuration_task, "Reset configuration", 256, NULL, 2, NULL);
 }
 
-homekit_characteristic_t switch_on = HOMEKIT_CHARACTERISTIC_(
-    ON, false, .callback=HOMEKIT_CHARACTERISTIC_CALLBACK(switch_on_callback)
-);
+homekit_characteristic_t switch_on = HOMEKIT_CHARACTERISTIC_(ON, false, .callback=HOMEKIT_CHARACTERISTIC_CALLBACK(switch_on_callback));
 
 void gpio_init() {
-    gpio_enable(led_gpio, GPIO_OUTPUT);
-    led_write(false);
-    gpio_enable(relay_gpio, GPIO_OUTPUT);
-    out = switch_on.value.bool_value;
-    led_write(out);
-    relay_write(out);
+    gpio_enable(LED_GPIO, GPIO_OUTPUT);
+    gpio_enable(RELAY_GPIO, GPIO_OUTPUT);
+    led_write(false, LED_GPIO);
+    relay_write(false, RELAY_GPIO);
 }
 
 void switch_on_callback(homekit_characteristic_t *_ch, homekit_value_t on, void *context) {
-    out = switch_on.value.bool_value;
-    led_write(out);
-    relay_write(out);
+    led_write(switch_on.value.bool_value, LED_GPIO);
+    relay_write(switch_on.value.bool_value, RELAY_GPIO);
 }
 
-void button_callback(uint8_t gpio, button_event_t event) {
-    switch (event) {
-        case button_event_single_press:
-            printf("Toggling relay\n");
-            switch_on.value.bool_value = !switch_on.value.bool_value;
-            out = switch_on.value.bool_value;
-            led_write(out);
-            relay_write(out);
-            homekit_characteristic_notify(&switch_on, switch_on.value);
-            break;
-        case button_event_long_press:
-            reset_configuration();
-            break;
-        default:
-            printf("Unknown button event: %d\n", event);
-    }
+void toggle_switch(const uint8_t gpio) {
+    printf("Toggle Switch manual\n");
+    switch_on.value.bool_value = !switch_on.value.bool_value;
+    led_write(switch_on.value.bool_value, gpio);
+    relay_write(switch_on.value.bool_value, gpio);
+    homekit_characteristic_notify(&switch_on, switch_on.value);
 }
 
 void switch_identify_task(void *_args) {
-    // We identify the mInI by Flashing it's LED.
+    // Identify the device by Flashing it's LED.
     for (int i=0; i<3; i++) {
         for (int j=0; j<2; j++) {
-            led_write(true);
+            led_write(true, LED_GPIO);
             vTaskDelay(100 / portTICK_PERIOD_MS);
-            led_write(false);
+            led_write(false, LED_GPIO);
             vTaskDelay(100 / portTICK_PERIOD_MS);
         }
-
         vTaskDelay(250 / portTICK_PERIOD_MS);
     }
-
-    led_write(false);
-
+    led_write(false, LED_GPIO);
     vTaskDelete(NULL);
 }
 
@@ -145,9 +107,9 @@ homekit_accessory_t *accessories[] = {
         HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]){
             &name,
             HOMEKIT_CHARACTERISTIC(MANUFACTURER, "Armo Ltd."),
-            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "001A1AVBG02P"),
-            HOMEKIT_CHARACTERISTIC(MODEL, "D1"),
-            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "0.1.8"),
+            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "016A1AVBG02P"),
+            HOMEKIT_CHARACTERISTIC(MODEL, "Sonoff"),
+            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "0.2.4"),
             HOMEKIT_CHARACTERISTIC(IDENTIFY, switch_identify),
             NULL
         }),
@@ -163,7 +125,7 @@ homekit_accessory_t *accessories[] = {
 
 homekit_server_config_t config = {
     .accessories = accessories,
-    .password = "111-11-111" //must be changed to be valid
+    .password = "320-11-132"
 };
 
 void on_wifi_ready() {
@@ -173,25 +135,20 @@ void on_wifi_ready() {
 void create_accessory_name() {
     uint8_t macaddr[6];
     sdk_wifi_get_macaddr(STATION_IF, macaddr);
-
     int name_len = snprintf(NULL, 0, "Switch-%02X%02X%02X",
                             macaddr[3], macaddr[4], macaddr[5]);
     char *name_value = malloc(name_len+1);
     snprintf(name_value, name_len+1, "Switch-%02X%02X%02X",
              macaddr[3], macaddr[4], macaddr[5]);
-
     name.value = HOMEKIT_STRING(name_value);
 }
 
 void user_init(void) {
     uart_set_baud(0, 115200);
-
     create_accessory_name();
-
-    wifi_config_init("switch", NULL, on_wifi_ready);
+    wifi_config_init("Switch", NULL, on_wifi_ready);
     gpio_init();
-
-    if (button_create(button_gpio, 0, 4000, button_callback)) {
-        printf("Failed to initialize button\n");
-    }
+    adv_button_create(BUTTON_GPIO);
+    adv_button_register_callback_fn(BUTTON_GPIO, toggle_switch, 1);
+    adv_button_register_callback_fn(BUTTON_GPIO, reset_configuration, 5);
 }
